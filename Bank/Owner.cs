18f784575375853322b6415628eb0402dee8d583
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-28 10:41:01 +0100

    owner

---
+++ Bank/Owner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal sealed class Owner
+    {
+        string name;
+        public Owner(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get => name; set => name = value; }
+    }
+}
