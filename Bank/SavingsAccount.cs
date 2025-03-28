8ccd9f76fc6f5945b27445db073471f0e9e03506
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-28 13:11:36 +0100

    butaságok

--- Bank/SavingsAccount.cs
+++ Bank/SavingsAccount.cs
@@ -8,7 +8,7 @@ namespace Bank
 {
     internal class SavingsAccount : BankAccount
     {
-        static double baseInterest;
+        static double baseInterest = 0.1;
         public double Interest { get; set; }
         public SavingsAccount(Owner owner) : base(owner)
         {
@@ -26,7 +26,7 @@ namespace Bank
         }
         public void AddInterest()
         {
-            Balance += Balance * (Interest / 100);
+            Balance += Balance * Interest;
         }
     }
 }
